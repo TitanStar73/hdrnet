a1955d2e7c68326e8589d741d24e8b77bca278b0
TitanStar73 <153201417+TitanStar73@users.noreply.github.com>
2025-11-18 20:47:03 +0530

    Update run.py

--- hdrnet/bin/run.py
+++ hdrnet/bin/run.py
@@ -29,6 +29,9 @@ import skimage.transform
 import sys
 import time
 import tensorflow as tf
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import models as models
 import utils as utils
